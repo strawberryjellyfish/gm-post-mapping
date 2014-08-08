a9efcf2d5fac23ab35bee0b6d16a2d146c25e04e
Robert Miller <robert@staff.mindvision.com.au>
2014-08-08 20:30:44 +0930

    Adding utility functions
    a heap of functions to hide and show markers and routes by id, category
    etc.
    
    Added id field to test data in map.html

--- map.js
+++ map.js
@@ -11,6 +11,8 @@ var sjMap = {
   routes: [],
   markers: [],
 
+  // setup functions
+
   initialize: function(){
     // set up map using data attribute settings or defaults
     this.mapElement = $('#map_canvas');
@@ -23,10 +25,38 @@ var sjMap = {
         this.mapElement.data('zoom') ?
           this.mapElement.data('zoom') :
           4,
+      min_zoom:
+        this.mapElement.data('min-zoom') ?
+          this.mapElement.data('min-zoom') :
+          1,
+      max_zoom:
+        this.mapElement.data('max-zoom') ?
+          this.mapElement.data('max-zoom') :
+          20,
       center:
         this.mapElement.data('center') ?
           new google.maps.LatLng(mapElement.data('center')) :
           new google.maps.LatLng(-23.6, 133.3),
+      constrain_ne:
+        this.mapElement.data('constrain_ne') ?
+          new google.maps.LatLng(mapElement.data('constrain_ne')) :
+          new google.maps.LatLng(6.122611334025562, -169.74488196250002),
+      constrain_sw:
+        this.mapElement.data('constrain_sw') ?
+          new google.maps.LatLng(mapElement.data('constrain_sw')) :
+          new google.maps.LatLng(-37.19162567748894, 77.04999999999995),
+      disableDoubleClickZoom:
+        this.mapElement.data('disable-double-click') ?
+          this.mapElement.data('disable-double-click') :
+          true,
+      draggable:
+        this.mapElement.data('draggable') ?
+          this.mapElement.data('draggable') :
+          true,
+      scrollable:
+        this.mapElement.data('scrollable') ?
+          this.mapElement.data('scrollable') :
+          true,
       mapTypeControl:
         this.mapElement.data('type-control') ?
           this.mapElement.data('type-control') :
@@ -88,6 +118,34 @@ var sjMap = {
     var postMarkers = this.mapElement.data('post-markers');
     if (postMarkers.length > 0)
       this.drawPostMarkers(postMarkers);
+
+    if (myOptions['constrain_ne'] && myOptions['constrain_sw']) {
+
+      var strictBounds = new google.maps.LatLngBounds(
+        myOptions['constrain_sw'], myOptions['constrain_ne']
+      );
+
+      google.maps.event.addListener(this.map, 'bounds_changed', function() {
+        if (strictBounds.contains(this.getCenter())) return;
+
+        // We're out of bounds - Move the map back within the bounds
+        var c = this.getCenter(),
+        x = c.lng(),
+        y = c.lat(),
+        maxX = strictBounds.getNorthEast().lng(),
+        maxY = strictBounds.getNorthEast().lat(),
+        minX = strictBounds.getSouthWest().lng(),
+        minY = strictBounds.getSouthWest().lat();
+
+        if (x < minX) x = minX;
+        if (x > maxX) x = maxX;
+        if (y < minY) y = minY;
+        if (y > maxY) y = maxY;
+
+        this.setCenter(new google.maps.LatLng(y, x));
+        console.log('center '+y+' '+x);
+      });
+    }
   },
 
   drawRoute: function(route) {
@@ -99,7 +157,7 @@ var sjMap = {
       var coords = new google.maps.LatLng( marker['lat'], marker['lng'] );
       markerCoordinates.push(coords);
       if (route['showMarkers'] == 'yes') {
-        this.addMarker( marker['name'], coords, marker['icon'], i, null, null, route['id'], null );
+        this.addMarker( marker['id'], marker['name'], coords, marker['icon'], i, null, null, 'route', route['id'], null );
       }
     }
     if (route['showRoute'] == 'yes') {
@@ -110,11 +168,35 @@ var sjMap = {
         strokeOpacity: stroke_style[1],
         strokeWeight: stroke_style[2],
         name: route['name'],
-        id: route['id']
+        id: route['id'],
+        pulse: function() {
+          var op = 1;  // initial opacity
+          var d = 0;
+          var c = 0;
+          var route = this;
+          var timer = setInterval(function () {
+            if (op <= 0.1) {
+              d = 1;
+            } else if (op >= 1) {
+              d = 0;
+              c += 1;
+            }
+            route.setOptions({ strokeOpacity: op});
+            if (d == 1) {
+              op += op * 0.25;
+            } else {
+              op -= op * 0.25;
+            }
+            if ( c >= 5) {
+              clearInterval(timer);
+              route.setOptions({ strokeOpacity: 1});
+            }
+          }, 50);
+        }
       });
       routeLine.setMap(this.map);
     }
-    this.routes.push(routeLine);
+    this.routes[routeLine.id] = routeLine;
   },
 
   drawPostMarkers: function(markers) {
@@ -123,17 +205,18 @@ var sjMap = {
       var marker = markers[i];
       var coords = new google.maps.LatLng(marker['lat'], marker['lng']);
       this.addMarker(
-        marker['name'], coords, marker['icon'], marker['index'],
-        marker['url'], marker['summary'], null, marker['category']
+        marker['id'], marker['name'], coords, marker['icon'], marker['index'],
+        marker['url'], marker['summary'], 'post', null, marker['category']
       );
     }
   },
 
-  addMarker: function(title, coords, icon, z, url, summary, route, category) {
+  addMarker: function(id, title, coords, icon, z, url, summary, type, route, category) {
     // add marker to map and store additional attributes to allow identifying
     // marker by route or category.
     var iconUrl = icon ? this.iconPath + icon + '.png' : this.iconPath + '0.png';
     var marker = new google.maps.Marker({
+      id: id,
       position: coords,
       map: this.map,
       icon: iconUrl,
@@ -141,6 +224,7 @@ var sjMap = {
       zIndex: z,
       url: url,
       html: summary,
+      type: type,
       route: route,
       category: category
     });
@@ -162,7 +246,106 @@ var sjMap = {
         sjMap.infoWindow.close();
       });
     }
+    this.markers[marker.id] = marker;
+  },
 
-    this.markers.push(marker);
-  }
+
+  // Utility functions
+
+  showRoute: function(routeId) {
+    if (routeId) sjMap.routes[routeId].setMap(sjMap.map);
+  },
+
+  hideRoute: function(routeId) {
+    if (routeId) sjMap.routes[routeId].setMap(null);
+  },
+
+  showRoutes: function(routeIds) {
+    if (routeIds) {
+      $.each( routeIds, function(index, value) {
+        sjMap.showRoute(value);
+      });
+    }
+  },
+
+  hideRoutes: function(routeIds) {
+    if (routeIds) {
+      $.each( routeIds, function(index, value) {
+        sjMap.hideRoute(value);
+      });
+    }
+  },
+
+  showMarker: function(markerId) {
+    if (markerId) sjMap.markers[markerId].setMap(sjMap.map);
+  },
+
+  hideMarker: function(markerId) {
+    if (markerId) sjMap.markers[markerId].setMap(null);
+  },
+
+  highlightMarker: function(markerId) {
+    // visually highlight the marker
+    // TODO: add some form of marker hilighting: colour border, animation?
+  },
+
+  showMarkers: function(markersIds) {
+    if (markerIds) {
+      $.each( markersIds, function(index, value) {
+        sjMap.showMarker(value);
+      });
+    }
+  },
+
+  hideMarkers: function(markersIds) {
+    if (markerIds) {
+      $.each( markersIds, function(index, value) {
+        sjMap.hideMarker(value);
+      });
+    }
+  },
+
+  showMarkersByCategory: function(categoryId) {
+    $.each( sjMap.markers, function(index, value) {
+      if (value && value.category == categoryId)
+        sjMap.showMarker(value.id);
+    });
+  },
+
+  hideMarkersByCategory: function(categoryId) {
+    $.each( sjMap.markers, function(index, value) {
+      if (value && value.category == categoryId)
+        sjMap.hideMarker(value.id);
+    });
+  },
+
+  showMarkersByRoute: function(routeId, showRoute, showPost) {
+    $.each( sjMap.markers, function(index, value) {
+      if ( value && value.route == routeId ) {
+        if (showRoute && value.type == 'route') sjMap.showMarker(value.id);
+        if (showPost && value.type == 'post') sjMap.showMarker(value.id);
+      }
+    });
+  },
+
+  hideMarkersByRoute: function(routeId, hideRoute, hidePost) {
+    $.each( sjMap.markers, function(index, value) {
+      if (value && value.route == routeId ) {
+        if (hideRoute && value.type == 'route') sjMap.hideMarker(value.id);
+        if (hidePost && value.type == 'post') sjMap.hideMarker(value.id);
+      }
+    });
+  },
+
+  // fadeRoute: function(routeId) {
+  //   var op = 1;  // initial opacity
+  //   var timer = setInterval(function () {
+  //     if (op <= 0.1){
+  //       clearInterval(timer);
+  //       sjMap.routes[routeId].setOptions({ strokeOpacity: 0});
+  //     }
+  //     sjMap.routes[routeId].setOptions({ strokeOpacity: op});
+  //     op -= op * 0.1;
+  //   }, 100);
+  // }
 }
